6eba325b893c70daf3e7ca0cd20a4514107a6a9f
bleo <bleo@mit.edu>
2022-11-07 16:30:45 -0500

    reactions work now

--- client/components/Reactions/ReactionRow.vue
+++ client/components/Reactions/ReactionRow.vue
@@ -2,16 +2,69 @@
 <!-- eslint-disable vue/singleline-html-element-content-newline -->
 <template>
   <div class="reaction-row">
-    <button
-      v-if="userReaction && userReaction.emotion === 'happy'"
-      @click="submitReaction('happy')"
-    >
-      🤩 {{ emotions.happy }}
-    </button>
-    <button>😢 {{ emotions.sad }}</button>
-    <button>😡 {{ emotions.angry }}</button>
-    <button>🤔 {{ emotions.confused }}</button>
-    <button>😱 {{ emotions.shocked }}</button>
+    <div v-if="signedIn">
+      <button
+        v-if="userReaction && userReaction.emotion === 'happy'"
+        class="current-reaction"
+        @click="removeReaction('happy')"
+      >
+        🤩 {{ emotions.happy }}
+      </button>
+      <button v-else @click="submitReaction('happy')">
+        🤩 {{ emotions.happy }}
+      </button>
+
+      <button
+        v-if="userReaction && userReaction.emotion === 'sad'"
+        class="current-reaction"
+        @click="removeReaction('sad')"
+      >
+        😢 {{ emotions.sad }}
+      </button>
+      <button v-else @click="submitReaction('sad')">
+        😢 {{ emotions.sad }}
+      </button>
+
+      <button
+        v-if="userReaction && userReaction.emotion === 'angry'"
+        class="current-reaction"
+        @click="removeReaction('angry')"
+      >
+        😡 {{ emotions.angry }}
+      </button>
+      <button v-else @click="submitReaction('angry')">
+        😡 {{ emotions.angry }}
+      </button>
+
+      <button
+        v-if="userReaction && userReaction.emotion === 'confused'"
+        class="current-reaction"
+        @click="removeReaction('confused')"
+      >
+        🤔 {{ emotions.confused }}
+      </button>
+      <button v-else @click="submitReaction('confused')">
+        🤔 {{ emotions.confused }}
+      </button>
+
+      <button
+        v-if="userReaction && userReaction.emotion === 'shocked'"
+        class="current-reaction"
+        @click="removeReaction('shocked')"
+      >
+        😱 {{ emotions.shocked }}
+      </button>
+      <button v-else @click="submitReaction('shocked')">
+        😱 {{ emotions.shocked }}
+      </button>
+    </div>
+    <div v-else>
+      <button>🤩 {{ emotions.happy }}</button>
+      <button>😢 {{ emotions.sad }}</button>
+      <button>😡 {{ emotions.angry }}</button>
+      <button>🤔 {{ emotions.confused }}</button>
+      <button>😱 {{ emotions.shocked }}</button>
+    </div>
   </div>
 </template>
 
@@ -33,6 +86,9 @@ export default {
     }
   },
   computed: {
+    /**
+     * Calculates the number of emotions for the passed down freet, if there is any
+     */
     emotions() {
       let newEmotions = {happy: 0, sad: 0, angry: 0, confused: 0, shocked: 0};
       if (this.reactions !== undefined && this.reactions.values) {
@@ -45,34 +101,66 @@ export default {
         return newEmotions;
       }
     },
+    /**
+     * Gets user's reaction of the passed down freet, if there is any
+     */
     userReaction() {
-      for (const reaction of this.reactions.values()) {
-        const author = reaction.username;
-        if (author === this.$store.state.username) {
-          return reaction;
+      if (this.reactions !== undefined && this.reactions.values) {
+        for (const reaction of this.reactions.values()) {
+          const author = reaction.username;
+          if (author === this.$store.state.username) {
+            return reaction;
+          }
         }
+        return null;
+      } else {
+        return null;
       }
-      return undefined;
     }
   },
   mounted() {},
   methods: {
-    hello() {},
     async submitReaction(emotion) {
       const url = `/api/reactions/${this.freetId}/`;
       const options = {
-        method: 'POST',
+        method: this.userReaction === null ? 'POST' : 'PUT', // POST if user has no previous reaction
         headers: {'Content-Type': 'application/json'},
         credentials: 'same-origin' // Sends express-session credentials with request
       };
       options.body = JSON.stringify({emotion: emotion});
+
       try {
         const r = await fetch(url, options);
         const res = await r.json();
         if (!r.ok) {
           throw new Error(res.error);
         }
-        this.$store.commit('updateReactions', res.reaction);
+        // Update Reactions state depending on POST or PUT request
+        options.method === 'POST'
+          ? this.$store.commit('addReaction', res.reaction)
+          : this.$store.commit('updateReaction', res.reaction);
+      } catch (e) {
+        this.$set(this.alerts, e, 'error');
+        setTimeout(() => this.$delete(this.alerts, e), 3000);
+      }
+    },
+
+    async removeReaction(emotion) {
+      const url = `/api/reactions/${this.freetId}/`;
+      const options = {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'same-origin' // Sends express-session credentials with request
+      };
+      options.body = JSON.stringify({emotion: emotion});
+
+      try {
+        const r = await fetch(url, options);
+        const res = await r.json();
+        if (!r.ok) {
+          throw new Error(res.error);
+        }
+        this.$store.commit('removeReaction', this.userReaction);
       } catch (e) {
         this.$set(this.alerts, e, 'error');
         setTimeout(() => this.$delete(this.alerts, e), 3000);
@@ -90,6 +178,12 @@ export default {
   gap: 4px;
 }
 
+.current-reaction {
+  background-color: rgb(178 225 255 / 39%);
+  border-color: #139ef4;
+  border-style: solid;
+  border-radius: 4px;
+}
 button {
   margin: 0 0px;
 }
--- client/store.ts
+++ client/store.ts
@@ -89,10 +89,10 @@ const store = new Vuex.Store({
       state.reactions = newReactions;
     },
 
-    updateReactions(state, reaction) {
+    addReaction(state, reaction) {
       /**
-       * Set the stored reactions to the provided reactions.
-       * @param reactions - Reactions to store
+       * Add the given reaction to the stored reactions.
+       * @param reaction - Reaction to store
        */
       const newReactions = JSON.parse(JSON.stringify(state.reactions)); // Copy to ensure no alliasing occurs;
       const freetId = reaction.freetId;
@@ -103,6 +103,46 @@ const store = new Vuex.Store({
       }
       state.reactions = newReactions;
     },
+
+    removeReaction(state, reaction) {
+      /**
+       * Remove the given reaction from the stored reactions.
+       * @param reaction - Reaction to store
+       */
+      const newReactions = JSON.parse(JSON.stringify(state.reactions)); // Copy to ensure no alliasing occurs;
+      const freetId = reaction.freetId;
+      if (freetId in newReactions) {
+        const freetReactions = newReactions[freetId];
+        const cleansedReactions = freetReactions.filter(
+          (freetReaction) => freetReaction._id !== reaction._id
+        );
+        newReactions[freetId] = cleansedReactions;
+      }
+      state.reactions = newReactions;
+    },
+
+    updateReaction(state, reaction) {
+      /**
+       * Update an exisiting user's reaction to a freetId.
+       * @param reaction - The new Reaction to store
+       */
+      const newReactions = JSON.parse(JSON.stringify(state.reactions)); // Copy to ensure no alliasing occurs;
+      const freetId = reaction.freetId;
+      const author = reaction.username;
+      if (freetId in newReactions) {
+        const freetReactions = newReactions[freetId];
+        // Search for reaction with a matching FreetId and has the given author
+        for (let i = 0; i < freetReactions.length; i++) {
+          if (freetReactions[i].username === author) {
+            freetReactions[i] = reaction; // replace old reaction with new reaction
+          }
+        }
+      } else {
+        newReactions[freetId] = new Array(reaction);
+      }
+      state.reactions = newReactions;
+    },
+
     async refreshFreets(state) {
       /**
        * Request the server for the currently available freets.
